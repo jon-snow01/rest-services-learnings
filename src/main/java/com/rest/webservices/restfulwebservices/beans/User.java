4e06998e50718861be19d672aab4cd2bbfbcd6c1
jon-snow01 <anand007abhishek@gmail.com>
2020-10-21 19:09:27 +0530

    Configured monitoring through actuator

--- src/main/java/com/rest/webservices/restfulwebservices/beans/User.java
+++ src/main/java/com/rest/webservices/restfulwebservices/beans/User.java
@@ -1,17 +1,23 @@
 package com.rest.webservices.restfulwebservices.beans;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "Attributes of Users")
 public class User {
 
     private Integer id;
 
     @Size(min=2, message = "Name should have at least 2 characters")
+    @ApiModelProperty(notes = "Name should have at least 2 characters")
     private String name;
 
     @Past(message = "Date shouldn't be in future")
+    @ApiModelProperty(notes = "Birthdate should always be in past")
     private Date birthDate;
 
     public User() {
--- src/main/java/com/rest/webservices/restfulwebservices/controllers/UserController.java
+++ src/main/java/com/rest/webservices/restfulwebservices/controllers/UserController.java
@@ -2,6 +2,9 @@ package com.rest.webservices.restfulwebservices.controllers;
 
 import com.rest.webservices.restfulwebservices.beans.User;
 import com.rest.webservices.restfulwebservices.services.UserDaoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,23 +17,27 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@Api
 public class UserController {
 
     @Autowired
     private UserDaoService userDaoService;
 
     @GetMapping("/users")
+    @ApiOperation(nickname = "GetAllUsers ",value = "This method is to get all the users")
     public List<User> retrieveAllUsers(){
         return userDaoService.findAll();
     }
 
     @GetMapping("users/{id}")
+    @ApiOperation(nickname = "GetUserById",value = "This method is to get user by their userId")
     public User retrieveUser(@PathVariable int id){
         User user = userDaoService.findOne(id);
         return user;
     }
 
     @PostMapping("/users")
+    @ApiOperation(nickname = "CreateNewUser", value = "Create a new User")
     public ResponseEntity<Object> createUser(@Valid @RequestBody User user){
         User savedUser = userDaoService.save(user);
 
@@ -43,7 +50,8 @@ public class UserController {
     }
 
     @DeleteMapping("users/{id}")
-    public User deleteUserById(@PathVariable int id){
+    @ApiOperation(nickname = "DeleteUser",value = "Delete a user by the Id")
+    public User deleteUserById(@PathVariable Integer id){
         User user = userDaoService.deleteUserById(id);
         return user;
     }
